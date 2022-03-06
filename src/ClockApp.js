96587032929685af5f5588401fb3c0d3bcd7da3b
LaietiMcLean <laia.marco@bootcamp-upgrade.com>
2022-03-06 12:06:21 +0100

    Digital Clock

---
+++ src/ClockApp.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { DigitalClock } from './Components/DigitalClock';
+
+import './index'
+import './index.css'
+
+export const App = () => {
+  return (
+    <>
+    <h3>Digital Clock:</h3>
+    <DigitalClock />
+    </>
+  )
+}
---
+++ src/Components/DigitalClock.jsx
@@ -0,0 +1,21 @@
+import React, { useEffect, useState } from 'react'
+
+import '../Components/DigitalClock.css'
+
+export const DigitalClock = () => {
+
+const [clockState, setClockState] = useState();
+
+useEffect(() => {
+  setInterval(() => {
+      const date = new Date();
+      setClockState(date.toLocaleTimeString());
+  }, 1000);
+}, []);
+
+    return (
+        <div className="DigitalClock">
+            <p>{clockState}</p>
+        </div>
+    )
+}
